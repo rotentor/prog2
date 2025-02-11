8bc3407e68a1d836d5c3ca40dc267deff73c075c
Jaime Ramirez <jramirod30@gmail.com>
2025-02-11 08:45:32 +0100

    resolución completo del ejercicio en la clase 11-2

--- EjSegmento/src/Segmento.java
+++ EjSegmento/src/Segmento.java
@@ -1,15 +1,55 @@
 
 public class Segmento {
 	private Punto a, b;
-	
+
 	public Segmento(Punto a, Punto b) {
 		this.a = a;
 		this.b = b;		
 	}
-	
+
 	public String toString() {
 		return "a: " + a.toString() + " b: " + b.toString();		
 	}
+
+	public double longitud(){
+		return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + 
+				Math.pow(a.getY() - b.getY(), 2));
+	}
+
+	public void desplazar(double dx, double dy) {
+		a.setX(a.getX()+dx);
+		a.setY(a.getY()+dy);
+		b.setX(b.getX()+dx);
+		b.setY(b.getY()+dy);
+	}
+	public Punto puntoMedio(){
+		Punto puntoMed = new Punto (0,0);
+		puntoMed.setX((a.getX()+b.getX())/2);
+		puntoMed.setY((a.getY()+b.getY())/2);
+		return puntoMed;
+	}
+
+	public Punto puntoMedio1(){
+		Punto puntoMed = new Punto ((a.getX()+b.getX())/2,
+				(a.getY()+b.getY())/2);
+
+		return puntoMed;
+
+		// también así:
+		//return new Punto ((a.getX()+b.getX())/2,
+		//		(a.getY()+b.getY())/2);
+	}
+
+	public boolean esIgual(Segmento s) {
+		return(a.esIgual(s.a) &&b.esIgual(s.b) || 
+				a.esIgual(s.b) &&b.esIgual(s.a));
+	}
+	
+	public Segmento (Segmento s) {
+		a= new Punto(s.a);
+		b= new Punto(s.b);
+	}
+	
 	
 
 }
