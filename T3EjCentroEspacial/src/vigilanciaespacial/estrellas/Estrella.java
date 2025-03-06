4092def94c85daa20cb89aafeb85025674a63cc6
Jaime Ramirez <jramirod30@gmail.com>
2025-03-06 10:42:42 +0100

    solución en 6-3

--- T3EjCentroEspacial/src/vigilanciaespacial/estrellas/Estrella.java
+++ T3EjCentroEspacial/src/vigilanciaespacial/estrellas/Estrella.java
@@ -1,5 +1,35 @@
 package vigilanciaespacial.estrellas;
 
 public class Estrella {
-
+	private String identificador;
+	private double campoMagnetico;
+	private int indiceColor;
+	
+	public Estrella(String identificador, double campoMagnetico, 
+			int indiceColor) {
+		this.campoMagnetico = campoMagnetico;
+		this.identificador = identificador;
+		this.indiceColor = indiceColor;
+	}
+	
+	public String getIdentificador() {
+		return identificador;
+	}
+	
+	public double getCampoMagnetico() {
+		return campoMagnetico;
+	}
+	
+	public int getIndiceColor() {
+		return indiceColor;
+	}
+	
+	public void desplazar() {
+		campoMagnetico -= 5.5;
+		indiceColor -= 1;
+	}
+	
+	public boolean esIgual(Estrella obj) {
+		return this.identificador.equals(obj.identificador); 
+	}
 }
