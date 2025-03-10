c57522eb9e7148557566ec00da8b4357310c1971
Jaime Ramirez <jramirod30@gmail.com>
2025-03-10 17:10:55 +0100

    solución completa vista el 10-3

--- T3EjCentroEspacial/src/vigilanciaespacial/CentroEspacial.java
+++ T3EjCentroEspacial/src/vigilanciaespacial/CentroEspacial.java
@@ -1,5 +1,43 @@
 package vigilanciaespacial;
 
 public class CentroEspacial {
-
+	private String identificador;
+	private SateliteArtificial[] satelitesSupervisados;
+	
+	public CentroEspacial(String identificador, 
+			SateliteArtificial[] satelites) {
+		this.identificador = identificador;
+		this.satelitesSupervisados = satelites;
+	}
+	
+	public String getIdentificador() {
+		return identificador;
+	}
+	
+	public String informacionEstrellas(){
+		String res = "";
+		for (int i = 0; i < satelitesSupervisados.length; i++) {
+			if (!satelitesSupervisados[i].hayAlertaTormentaMagnetica()) {
+				res += satelitesSupervisados[i].getEstrella().getIdentificador() 
+						+ " (" + satelitesSupervisados[i].darTemperatura() + ")\n";
+			}
+			else {
+				res += satelitesSupervisados[i].getEstrella().getIdentificador()
+						+ " (" + satelitesSupervisados[i].darTemperatura() 
+						+ ") ALERTA!\n";
+			}
+		}
+		return res;
+	}
+	
+	public boolean comprobarSatelites() {
+		boolean repetidos = false;
+		for(int i=0; i<satelitesSupervisados.length - 1 && !repetidos; i++) {
+			for(int j = i+1; j<satelitesSupervisados.length && !repetidos; j++) {
+				repetidos = (satelitesSupervisados[i].esIgual(satelitesSupervisados[j]));
+			}
+		}
+		return satelitesSupervisados.length > 0 && !repetidos;
+	}
+	
 }
--- T3EjCentroEspacial/src/vigilanciaespacial/SateliteArtificial.java
+++ T3EjCentroEspacial/src/vigilanciaespacial/SateliteArtificial.java
@@ -1,5 +1,40 @@
 package vigilanciaespacial;
 
-public class SateliteArtificial {
+import vigilanciaespacial.estrellas.Estrella;
 
+public class SateliteArtificial {
+	private static int contador = 0;
+	private String identificador;
+	private Estrella estrellaMonitorizada;
+	
+	public SateliteArtificial(Estrella estrella) {
+		contador++;
+		this.identificador="Sat"+contador;
+		this.estrellaMonitorizada=estrella;		
+	}
+	
+	public String getIdentificador(){
+		return identificador;
+	}
+	
+	public Estrella getEstrella () {
+		return estrellaMonitorizada;
+	}
+	
+	public void setEstrella(Estrella e) {
+		estrellaMonitorizada = e;
+	}
+	
+	public double darTemperatura() {
+		return estrellaMonitorizada.getIndiceColor() * 2.1;
+	}
+	
+	public boolean hayAlertaTormentaMagnetica(){
+		return estrellaMonitorizada.getCampoMagnetico()>=1000;
+	}
+	
+	public boolean esIgual(SateliteArtificial satelite) {
+		return this.estrellaMonitorizada.esIgual
+				(satelite.estrellaMonitorizada);
+	}
 }
